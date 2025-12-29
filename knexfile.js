b05d8b1bebb28a79559995a3e37d29f1ca0918b9
prcass <prcass@users.noreply.github.com>
2025-12-29 00:06:01 -0500

    Fix knexfile config

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export default {
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-      host: 'localhost',
-      port: 5432,
-      user: 'postgres',
-      password: 'postgres',
-      database: 'cardsnipe_dev'
-    },
-    pool: { min: 2, max: 10 },
-    migrations: {
-      directory: './src/db/migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + '?sslmode=require',
-    pool: { min: 2, max: 20 },
-    migrations: {
-      directory: './src/db/migrations'
-    }
+const config = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/cardsnipe_dev',
+  pool: { min: 2, max: 10 },
+  migrations: {
+    directory: './src/db/migrations'
   }
 };
+
+export default config;
